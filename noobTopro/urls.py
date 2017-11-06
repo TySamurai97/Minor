907efe3d172d464faf7f7f68acc1afebe69bf507
tyjackx@gmail.com <tyjackx@gmail.com>
2017-11-06 14:13:07 +0530

    updated noobtopro.urls.py

--- noobTopro/urls.py
+++ noobTopro/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^cp/', include('cp.urls'))
+    url(r'^cp/', include('cp.urls')),
+    url(r'', include('cp.urls'))
 ]
